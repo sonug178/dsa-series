8f0d5b63732869c155723dd0fd283d12e4d5a1ea
SONU <sonugandele302@gmail.com>
2026-07-10 14:53:27 +0530

    Time: 0 ms (100%), Space: 28.2 MB (67.7%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -18,7 +18,7 @@ public:
         }
         int cnt   = 0;
         for(int i =0;i<nums.size();i++){
-            if(nums[i==el]) cnt++;
+            if(nums[i]==el) cnt++;
         }
         if(cnt>nums.size()/2) return el;
         return 0;
