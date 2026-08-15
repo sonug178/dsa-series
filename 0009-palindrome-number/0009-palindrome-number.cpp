da51e7b45f090bca6121c451323e25535cc27424
SONU <sonugandele302@gmail.com>
2026-08-15 10:38:39 +0530

    Time: 3 ms (44.05%), Space: 10.8 MB (16.55%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPalindrome(int x){
-        if(x<0) return false;
+      if(x<0) return false;
         string n = to_string(x);
         int m = n.length();
         int j = m-1;
@@ -14,6 +14,5 @@ public:
             else return false;
         }
         return true;
-        
     }
 };
